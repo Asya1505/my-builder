d682ab88599a3fc02ee00360ef63275ac23c817c
Asya Muksimova <asya1505@gmail.com>
2017-07-02 16:38:55 +0500

    дз3 js

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,23 @@
 (function() {
   'use strict';
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
+  // setTimeout(function() {
+  //   document.querySelector('.greating_picture').classList.add('m--show');
+  // }, 1000);
+    var card = document.querySelector(".card"),
+        btn = document.querySelector(".avtorization");
+
+    btn.addEventListener( "click", flipFn);
+
+    function flipFn(){
+        var c = card.classList;
+
+        c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+    }
 })();
+
+$(document).ready(function(){
+  $('.hamburger').click(function() {
+      $(this).toggleClass('active');
+  });
+});
