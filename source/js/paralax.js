6fe14f3a7f338fb36be26e44bbafede232e43de4
Asya Muksimova <asya1505@gmail.com>
2017-07-09 21:01:28 +0500

    дз4

---
+++ source/js/paralax.js
@@ -0,0 +1,11 @@
+(function() {
+    var parallaxContainer = document.getElementById('parallax'),
+        layers = parallaxContainer.children;
+
+    var moveLayers = function (e) {
+        console.log('ok');
+
+    };
+
+    window.addEventListener('mousemove', moveLayers);
+})();
